2b90ba5e7bcad468bc2ef0782cc0642d800379f5
muhammednihan <43811672+muhammednihan@users.noreply.github.com>
2022-08-25 11:49:44 +0530

    Expt 2
    Lifecycle

--- MainActivity.java
+++ MainActivity.java
@@ -1,47 +1,53 @@
-package com.example.game1;
+package com.example.applifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import  android.widget.Toast;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView t1,t2,t3;
-    int n1,n2,result;
-    Button b;
-    EditText e;
-    Random rand;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t1=(TextView) findViewById(R.id.textView1);
-        t2=(TextView) findViewById(R.id.textView);
-        t3=(TextView) findViewById(R.id.textView3);
-        e=(EditText) findViewById(R.id.editTextTextPersonName);
-        b=(Button) findViewById(R.id.button);
-        rand=new Random();
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                n1=Integer.parseInt(t1.getText().toString());
-                n2=Integer.parseInt(t2.getText().toString());
-                result=Integer.parseInt(e.getText().toString());
-                if(n1+n2==result)
-                {
-                    t3.setText("You Won...");
-                    t1.setText(""+rand.nextInt(500));
-                    t2.setText(""+rand.nextInt(500));
-                }
-                else
-                {
-                    t3.setText("Better Luck Next Time...");
-                }
-            }
-        });
+        Toast toast = Toast.makeText(getApplicationContext(), "onCreate Called", Toast.LENGTH_LONG);
+        toast.show();
+    }
 
+    protected void onStart() {
+        super.onStart();
+        Toast toast = Toast.makeText(getApplicationContext(), "onStart Called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast toast = Toast.makeText(getApplicationContext(), "onRestart Called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast toast = Toast.makeText(getApplicationContext(), "onPause Called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast toast = Toast.makeText(getApplicationContext(), "onResume Called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast toast = Toast.makeText(getApplicationContext(), "onStop Called", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast toast = Toast.makeText(getApplicationContext(), "onDestroy Called", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
