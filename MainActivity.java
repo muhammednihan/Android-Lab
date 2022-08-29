5a311eea2fb1d13eb3ddeaa149675315cd69c6dd
muhammednihan <43811672+muhammednihan@users.noreply.github.com>
2022-08-29 14:58:27 +0530

    Expt 2
    Updated with log

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,21 @@
-package com.example.applifecycle;
+package com.example.alc;
 
 import androidx.appcompat.app.AppCompatActivity;
 import  android.widget.Toast;
+import android.util.Log;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String MAIN_ACTIVITY_TAG = MainActivity.class.getSimpleName();
+
+    private void showLog(String text){
+        Log.d(MAIN_ACTIVITY_TAG, text);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        showLog("Activity Created");
         setContentView(R.layout.activity_main);
         Toast toast = Toast.makeText(getApplicationContext(), "onCreate Called", Toast.LENGTH_LONG);
         toast.show();
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onStart() {
         super.onStart();
+        showLog("Activity started");
         Toast toast = Toast.makeText(getApplicationContext(), "onStart Called", Toast.LENGTH_LONG);
         toast.show();
     }
@@ -23,30 +31,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+        showLog("Activity restarted");
         Toast toast = Toast.makeText(getApplicationContext(), "onRestart Called", Toast.LENGTH_LONG);
         toast.show();
     }
 
     protected void onPause() {
         super.onPause();
+        showLog("Activity paused");
         Toast toast = Toast.makeText(getApplicationContext(), "onPause Called", Toast.LENGTH_LONG);
         toast.show();
     }
 
     protected void onResume() {
         super.onResume();
+        showLog("Activity resumed");
         Toast toast = Toast.makeText(getApplicationContext(), "onResume Called", Toast.LENGTH_LONG);
         toast.show();
     }
 
     protected void onStop() {
         super.onStop();
+        showLog("Activity stopped");
         Toast toast = Toast.makeText(getApplicationContext(), "onStop Called", Toast.LENGTH_LONG);
         toast.show();
     }
 
     protected void onDestroy() {
         super.onDestroy();
+        showLog("Activity had destroyed");
         Toast toast = Toast.makeText(getApplicationContext(), "onDestroy Called", Toast.LENGTH_LONG);
         toast.show();
     }
